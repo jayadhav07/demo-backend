ae0a0c6040cf4a2467c126f177df77ca5ced2b57
jayadhav07 <jayadhav786@gmail.com>
2025-08-30 22:24:07 +0530

    updated

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const app = express();
+
+app.listen(3000, () => {
+  console.log("server is running on PORT 3000");
+});
